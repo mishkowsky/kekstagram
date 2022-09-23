0a78106803e31f6ae7e9fcb4125988ba80b3f5c5
mishkowsky <aleksandrov.m2542@gmail.com>
2022-09-23 22:20:31 +0300

    git

--- js/main.js
+++
@@ -1,8 +0,0 @@
-function randomFromRange(min, max) {
-  if (min > max) { return NaN; }
-  return Math.floor(Math.random() * (max + 1 - min) + min);
-}
-
-function checkLenght(string, maxLength) {
-  return string.length <= maxLength;
-}
