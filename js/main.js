a92a12ad78ebce89eba2a679c7587f15d8a2e4aa
mishkowsky <aleksandrov.m2542@gmail.com>
2022-09-25 03:04:27 +0300

    module2-task1 #2

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
 //src: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#getting_a_random_integer_between_two_values_inclusive
-function randomFromRange(min, max) {
-  if (min > max) { return NaN; }
+const getRandomIntFromRange = (min, max) => {
+  if (min > max) {
+    throw new Error('Invalid arguments: upper limit is below the lower limit');
+  }
+  if (min < 0) {
+    throw new Error('Invalid arguments: range cannot be negative');
+  }
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max + 1 - min) + min);
-}
+};
 
-function checkLenght(string, maxLength) {
-  return string.length <= maxLength;
-}
 
-randomFromRange(1, 10);
+const checkLenght = (value, maxLength) => String(value).length <= maxLength;
+
+getRandomIntFromRange(1, 1000);
 checkLenght('some string', 10);
